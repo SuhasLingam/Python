a8e8b60eeeb31e5a81d63421d32e90d4433f561f
Suhas Srinivas Lingam <95020208+SuhasLingam@users.noreply.github.com>
2023-01-24 11:25:38 +0530

    classes :white_check_mark:

---
+++ class.py
@@ -0,0 +1,17 @@
+#classes and objects
+
+class car:
+    somename = "Supercars"
+    
+    def __init__(self,name,speed,cost):
+        self.name = name
+        self.speed = speed
+        self.cost = cost
+
+x = car("Supra" , 280 , 75)        
+y = car("lambo" , 300 , 12)
+
+print()
+
+
+    
