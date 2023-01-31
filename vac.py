b957761a4066248f046c7d21c66c5d5b6fd99dc5
Suhas Srinivas Lingam <95020208+SuhasLingam@users.noreply.github.com>
2023-01-31 11:08:26 +0530

    Vac :white_check_mark:

---
+++ vac.py
@@ -0,0 +1,32 @@
+# 5
+
+#year = int(input("Enter the Year :"))
+#if year%4 == 0:
+#    print('Leap YEar')
+#else:
+#    print('No')
+
+# 4
+#string = (input("Enter string :"))
+#print(string.lower())
+
+#2
+#n1 = int(input("Enter the num :"))
+#n2 = int(input("Enter the num :"))
+#n3 = int(input("Enter the num :"))
+#if n1<n2<n3:
+#    print('N3 greater')
+#elif n2<n3<n1:
+#    print('N2')
+#else:
+#   print('N2')
+
+#1
+#num = 0
+#for i in range(1,102,1):
+#    num = num+i
+#    print(num)
+
+#print(sum(range(1,102)))
+
+
