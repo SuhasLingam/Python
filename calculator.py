27fde09b688e72e468c73cc68a884e8f84b3fc9b
Suhas Srinivas Lingam <95020208+SuhasLingam@users.noreply.github.com>
2023-02-25 00:30:11 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,24 +1,18 @@
 print("This is calculator")
+
+ x = int(input("Enter 1st number : "))
+ y = int(input("Enter 2nd number : "))
+
 op = input("Enter the operrator +,-,%,/,* : ")
 if op == "+":
-    x = int(input("Enter 1st number : "))
-    y = int(input("Enter 2nd number : "))
     print(x+y)
 elif op == "-":
-    x = int(input("Enter 1st number : "))
-    y = int(input("Enter 2nd number : "))
     print(x-y)
 elif op == "%":
-    x = int(input("Enter 1st number : "))
-    y = int(input("Enter 2nd number : "))
     print(x%y)    
 elif op == "/":
-    x = int(input("Enter 1st number : "))
-    y = int(input("Enter 2nd number : "))
     print(x/y)
 elif op == "*":
-    x = int(input("Enter 1st number : "))
-    y = int(input("Enter 2nd number : "))
     print(x*y)    
 else:
     print("Invalid");
